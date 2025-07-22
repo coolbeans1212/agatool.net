e6dc66849044578883fe127fa51d789f5472a836
Matei sHomePage <matei@mateishome.page>
2025-07-22 17:20:44 +0100

    yeahhhh this is being scratched

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AgaTool</title>
+        <meta name="description" content="AgaTool - A multitasking bookmarklet.">
+        <link rel="icon" href="https://emoji.slack-edge.com/T0266FRGM/aga/9e87e28f51c6caa8.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="/styles.css?rand=<?php echo rand(0, 2147483647); ?>">
+    </head>
+    <body>
+        <h1>AgaTool</h1>
+        <div class="horizontal-row">
+            <div class="text">
+                <h2>AgaTool is... useful</h2>
+                <p>AgaTool has several tools to help you be more productive while browsing the web, such as a notepad, calculator, AI and iframe browser<span class="headnote">1</span>.</p>
+                <span class="footnote">1: AI and iframe browser subject to availability.</span>
+            </div>
+            <div class="image"><img src="/files/images/graphic1.png" alt="A screenshot of the AgaTool bookmarklet running"></div>
+        </div>
+        <div class="horizontal-row">
+            <div class="text">
+                <h2>AgaTool is... intelligent</h2>
+                <p>AgaTool will remember where everything is on a web page, so that when you navigate to a different subpage, you can pick up right where you left off.</p>
+        </div>
+    </body>
