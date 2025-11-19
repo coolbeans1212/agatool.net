c5a811eea5c3ce8177e614c857ff451a7d264278
Matei sHomePage <matei@mateishome.page>
2025-11-19 20:06:30 +0000

    CORS more like kys ✌️

--- integration/model.php
+++ integration/model.php
@@ -1,4 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers:  *');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    die(); // CORS makes me want to kms, like if you agree
+}
 $env = parse_ini_file(__DIR__ . '/../.env');
 if ($env['MODEL']) {
     echo $env['MODEL'];
